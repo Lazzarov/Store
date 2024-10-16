7d7dd4c8fa6a1d444a9efa3feb687ee42d1ac587
Lazzarov <burminovid@gmail.com>
2024-10-17 00:26:29 +0300

    commit

--- main/views.py
+++ main/views.py
@@ -6,10 +6,16 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     context = {
-        'title': 'Home Page',
-        'content': 'Главная страница'
+        'title': 'Home - Главная',
+        'content': 'Магазин мебели HOME'
 
     }
     return render(request, 'main/index.html', context)
 def about(request):
-    return HttpResponse("about page")
+    context = {
+        'title': 'Home - О нас',
+        'content': 'О нас',
+        'text_on_page': 'Магазин мебели "HOME" предлагает стильную и качественную мебель для дома и офиса. В ассортименте – удобные диваны, кровати, шкафы и столы от ведущих производителей. Мы гарантируем долговечность каждой покупки и помогаем подобрать мебель, создавая уют в вашем интерьере.'
+
+    }
+    return render(request, 'main/about.html', context)
