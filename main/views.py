60ce4ed4a48e639e9a72dde9dab3582eb47b154c
Lazzarov <burminovid@gmail.com>
2024-10-16 22:17:48 +0300

    commit

--- main/views.py
+++ main/views.py
@@ -1,8 +1,15 @@
+from lib2to3.fixes.fix_input import context
+
 from django.http import  HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'main/index.html')
+    context = {
+        'title': 'Home Page',
+        'content': 'Главная страница'
+
+    }
+    return render(request, 'main/index.html', context)
 def about(request):
     return HttpResponse("about page")
